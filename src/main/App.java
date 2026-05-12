a7ba2a930350aa4d2c8d3457b0de459668a8e0e2
Yimmer Enrique Mezu Castro <carlosstivengarciarosada@gmail.com>
2026-05-11 20:58:33 -0500

    Actividad 2 Carlos Stiven Garcia
    En este caso lleve a cabo
    
    Una clase abstracta es un "contrato". No puedes crear un Servicio a secas, pero obligas a que todos los servicios reales tengan un precio y una forma de mostrarse.
    
    Creamos un servicio específico que hereda de la clase padre.
    
    Otro servicio con una lógica de precio totalmente distinta.
    
    Aolicando Herencia y Polimorfismo

--- src/main/App.java
+++ src/main/App.java
@@ -1,23 +1,27 @@
-
-
-import modelos.Cliente;
-import excepciones.ClienteInvalidoException;
+import servicios.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("=== INICIO DE PRUEBA CORTA ===");
+        System.out.println("=== PRUEBA DE HERENCIA Y POLIMORFISMO ===");
 
-        try {
-            // Caso 1: Todo bien
-            Cliente c1 = new Cliente(1, "Yimme", "yimme@correo.com");
-            System.out.println("EXITO: " + c1.getDetalles());
+        // Creamos una lista de la clase abstracta
+        List<Servicio> carritoDeServicios = new ArrayList<>();
 
-            // Caso 2: Forzar el error para ver si el Log funciona
-            System.out.println("\nProbando validación de email...");
-            Cliente c2 = new Cliente(2, "Test Error", "correo-sin-arroba");
-            
-        } catch (ClienteInvalidoException e) {
-            System.err.println("CAPTURA CORRECTA: " + e.getMessage());
+        // Agregamos diferentes hijos (Polimorfismo en acción)
+        carritoDeServicios.add(new ServicioHospedaje("Habitación Suite", 100.0, 3));
+        carritoDeServicios.add(new ServicioSpa("Sesión Relax", 40.0, true));
+        carritoDeServicios.add(new ServicioSpa("Sauna Básico", 20.0, false));
+
+        double totalAcumulado = 0;
+
+        for (Servicio s : carritoDeServicios) {
+            double precioFinal = s.calcularPrecioFinal();
+            System.out.println(s.getInformacion() + " -> Total: $" + precioFinal);
+            totalAcumulado += precioFinal;
         }
+
+        System.out.println("\nTOTAL A PAGAR POR EL CLIENTE: $" + totalAcumulado);
     }
 }
--- src/main/servicios/Servicio.java
+++ src/main/servicios/Servicio.java
@@ -1,5 +1,18 @@
 package servicios;
 
-public class Servicio {
-    
+public abstract class Servicio {
+    protected String nombre;
+    protected double precioBase;
+
+    public Servicio(String nombre, double precioBase) {
+        this.nombre = nombre;
+        this.precioBase = precioBase;
+    }
+
+    // Método abstracto: cada servicio calculará su precio de forma distinta (Polimorfismo)
+    public abstract double calcularPrecioFinal();
+
+    public String getInformacion() {
+        return "Servicio: " + nombre + " | Precio Base: $" + precioBase;
+    }
 }
